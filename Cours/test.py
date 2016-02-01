8f0a33ffc3f4dffd255e744d3726565f9a5563d3
NOVEL Mathias <novelm@hostname.iut2.upmf-grenoble.fr>
2016-02-01 15:57:02 +0100

    Credits

--- Cours/test.py
+++ Cours/test.py
@@ -4,12 +4,30 @@ import os
 
 pygame.init()
 
-#os.environ['SDL_VIDEO_CENTERED'] = '1'
-size = width, height = 640, 480
-screen = pygame.display.set_mode(size)
+width = 1200
+height = 910
 
+#os.environ['SDL_VIDEO_CENTERED'] = '1'
+size = width, height
+screen = pygame.display.set_mode(size)
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+clock = pygame.time.Clock()
+
+
+text = pygame.font.SysFont("Arial",50)
+t = text.render("TEST",True,(255,255,255))
+
+x = width/2
+y = height
+done = False
 # Boucle de jeu
-while 1:
+while not done:
     for event in pygame.event.get():
     	if event.type == pygame.QUIT:
-    	    sys.exit()
+    	    done = True
+	print 'g'
+	screen.fill((0,0,0))
+	y-=1
+	screen.blit(t,(x,y))
+	pygame.display.flip()
+	clock.tick(60)
