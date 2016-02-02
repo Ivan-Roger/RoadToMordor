bad11d04655cf0682fa23981fc0f9d94ee4f71c8
3mixam <mgmaximegermain3@gmail.com>
2016-02-02 15:48:48 +0100

    Create IA.py

---
+++ IA.py
@@ -0,0 +1,38 @@
+import random
+import os
+import pygame
+import Grille
+
+def tour_IA(self,grille):
+	alea = random.randrange(100)
+	if(Grille.nb_tour <= 5):
+		if (alea < 50):
+			Grille.generer_homme()
+		else:
+			Grille.generer_tour()
+	if(Grille.nb_tour <= 10 and Grille.nb_tour > 5 ):
+		if (alea < 60):
+			Grille.generer_homme()
+		elif( alea > 89 ):
+			Grille.generer_upgrade()
+		else:
+			Grille.generer_tour()
+	if(Grille.nb_tour <= 15 and Grille.nb_tour > 10 ):
+		if (alea < 60):
+			Grille.generer_homme()
+		elif( alea > 79 ):
+			Grille.generer_upgrade()
+		else:
+			Grille.generer_tour
+	if(Grille.nb_tour <= 25 and Grille.nb_tour > 15 ):
+		if (alea < 70):
+			Grille.generer_homme()
+		elif( alea > 79 ):
+			Grille.generer_upgrade()
+		else:
+			Grille.generer_tour
+	if(Grille.nb_tour > 25 ):
+		if (alea < 60):
+			Grille.generer_homme()
+		else:
+			Grille.generer_upgrade()
