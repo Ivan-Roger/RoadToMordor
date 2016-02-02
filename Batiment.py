1c813eac6f0cbe0f946fa6a3b7d38a2bc7e85443
NOVEL Mathias <novelm@hostname.iut2.upmf-grenoble.fr>
2016-02-02 14:41:16 +0100

    batiment

---
+++ Batiment.py
@@ -0,0 +1,14 @@
+
+
+class Batiment:
+
+		def __init__(self,id_tour,joueur,prix,degats,portee,descr):
+			self.id_tour = id_tour
+			self.joueur = joueur
+			self.niveau = 1
+			self.prix = prix
+			self.degats = degats
+			self.portee = portee
+			self.descr = descr
+
+		def 
